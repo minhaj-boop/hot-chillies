e41439884b42b582c66743bf3988124cc10bc7c4
minhaj-boop <imminhajj@gmail.com>
2023-01-05 21:10:57 +0600

    initial router set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home/Home';
+import Account from './Pages/Account/Account';
+import Contact from './Pages/Contact/Contact';
+import Home from './Pages/Home/Home';
+import Menu from './Pages/Menu/Menu';
+import NewsAndPromotions from './Pages/NewsAndPromotions/NewsAndPromotions';
+import Outlet from './Pages/Outlet/Outlet';
+import PageNotFound from './Pages/PageNotFound/PageNotFound';
+import ShopOnline from './Pages/ShopOnline/ShopOnline';
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+
+          <Route path="/home" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/outlet" element={<Outlet />} />
+          <Route path="/shop-online" element={<ShopOnline />} />
+          <Route path="/news-and-promotions" element={<NewsAndPromotions />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/account" element={<Account />} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+
+        </Routes>
+      </Router>
     </>
   );
 }
