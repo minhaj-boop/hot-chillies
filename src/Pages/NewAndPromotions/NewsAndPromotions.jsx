95d6a8641b8254f6c19756c39e711296afd5344c
minhaj-boop <imminhajj@gmail.com>
2023-01-05 20:02:08 +0600

    file structure rearranged

---
+++ src/Pages/NewAndPromotions/NewsAndPromotions.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewsAndPromotions = () => {
+  return (
+    <div>
+      <h1>This is News and Promotions</h1>
+    </div>
+  );
+};
+
+export default NewsAndPromotions;
---
+++ src/Pages/ShopOnline/ShopOnline.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShopOnline = () => {
+  return (
+    <div>
+      <h1>This is shoponline</h1>
+    </div>
+  );
+};
+
+export default ShopOnline;
